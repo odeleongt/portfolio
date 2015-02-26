199e507b54eb6d7a5f54e82ec95da99c9b0d8080
odeleongt [nb] <odeleongt@gmail.com>
2015-02-26 08:43:17 -0600

    Outline for data science specialization portfolio

--- ui.R
+++ ui.R
@@ -1,7 +1,31 @@
 
 shinyUI(
-  bootstrapPage(
-    numericInput('n', 'Number of obs', 100),
-    plotOutput('plot')
+  fluidPage(
+    headerPanel(title = "Especialización de datos en Coursera"),
+    tabsetPanel(
+      tabPanel("General", 
+               fluidRow(
+                 wellPanel(
+                   fluidRow(
+                     column(1, numericInput("n", "Number of obs", 100)),
+                     column(3, ""),
+                     column(3, ""),
+                     column(3, "")
+                   ))),
+               fluidRow(
+                 column(6, plotOutput("plot"))
+               )),
+      tabPanel("Data scientist toolbox"),
+      tabPanel("R programming"),
+      tabPanel("Getting and Cleaning Data"),
+      tabPanel("Exploratory Data Analysis"),
+      tabPanel("Reproducible research"),
+      tabPanel("Statistical Inference"),
+      tabPanel("Regression Models"),
+      tabPanel("Practical Machine Learning"),
+      tabPanel("Developing Data Products"),
+      tabPanel("Data Science Capstone")
+      
+    )
   )
 )
