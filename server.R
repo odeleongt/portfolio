c0fcef1fa79de01b7178f0a1df59fe04a3fa1e7b
odeleongt [nb] <odeleongt@gmail.com>
2015-02-26 17:14:30 -0600

    navListPanel (side) and reactive template for courses

--- server.R
+++ server.R
@@ -1,6 +1,77 @@
+# Define courses
+courses <- data.frame(stringsAsFactors = FALSE,
+                      course = c("Data scientist toolbox",
+                                 "R programming",
+                                 "Getting and Cleaning Data",
+                                 "Exploratory Data Analysis",
+                                 "Reproducible research",
+                                 "Statistical Inference",
+                                 "Regression Models",
+                                 "Practical Machine Learning",
+                                 "Developing Data Products",
+                                 "Data Science Capstone"),
+                      num = 1:10
+)
 
 shinyServer(
   function(input, output) {
-    output$plot <- renderPlot({ hist(runif(input$n)) })
+    val <- reactive({
+      n <- paste0("n", courses$num[courses$course == input$course])
+      paste(
+        paste("Course:", input$course),
+        paste("Value:", input[[n]]),
+        sep = " --- "
+      )
+    })
+    
+    # Data scientist toolbox
+    output$text1 <- renderText({
+      val()
+    })
+    
+    # R programming
+    output$text2 <- renderText({
+      val()
+    })
+    
+    # Getting and Cleaning Data
+    output$text3 <- renderText({
+      val()
+    })
+    
+    # Exploratory Data Analysis
+    output$text4 <- renderText({
+      val()
+    })
+    
+    # Reproducible research
+    output$text5 <- renderText({
+      val()
+    })
+    
+    # Statistical Inference
+    output$text6 <- renderText({
+      val()
+    })
+    
+    # Regression Models
+    output$text7 <- renderText({
+      val()
+    })
+    
+    # Practical Machine Learning
+    output$text8 <- renderText({
+      val()
+    })
+    
+    # Developing Data Products
+    output$text9 <- renderText({
+      val()
+    })
+    
+    # Data Science Capstone
+    output$text10 <- renderText({
+      val()
+    })
   }
 )
--- ui.R
+++ ui.R
@@ -1,31 +1,102 @@
 
 shinyUI(
-  fluidPage(
-    headerPanel(title = "Especialización de datos en Coursera"),
-    tabsetPanel(
-      tabPanel("General", 
-               fluidRow(
-                 wellPanel(
+  fluidPage(title = "Especialización de datos en Coursera",
+    tabsetPanel(selected = "Cursos",
+      tabPanel("General", includeMarkdown("./content/00_General.md")),
+      tabPanel("Cursos",
+        navlistPanel(id = "course", widths = c(2, 9),
+                     
+          tabPanel(title = "Data scientist toolbox",
+                   includeMarkdown("./content/01_Data scientist toolbox.md"),
                    fluidRow(
-                     column(1, numericInput("n", "Number of obs", 100)),
-                     column(3, ""),
-                     column(3, ""),
-                     column(3, "")
-                   ))),
-               fluidRow(
-                 column(6, plotOutput("plot"))
-               )),
-      tabPanel("Data scientist toolbox"),
-      tabPanel("R programming"),
-      tabPanel("Getting and Cleaning Data"),
-      tabPanel("Exploratory Data Analysis"),
-      tabPanel("Reproducible research"),
-      tabPanel("Statistical Inference"),
-      tabPanel("Regression Models"),
-      tabPanel("Practical Machine Learning"),
-      tabPanel("Developing Data Products"),
-      tabPanel("Data Science Capstone")
-      
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n1", "Number of obs", 100)))),
+                     column(9, textOutput("text1")))),
+          
+          tabPanel("R programming",
+                   includeMarkdown("./content/02_R programming.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n2", "Number of obs", 100)))),
+                     column(9, textOutput("text2")))),
+          
+          tabPanel("Getting and Cleaning Data",
+                   includeMarkdown("./content/03_Getting and Cleaning Data.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n3", "Number of obs", 100)))),
+                     column(9, textOutput("text3")))),
+          
+          tabPanel("Exploratory Data Analysis",
+                   includeMarkdown("./content/04_Exploratory Data Analysis.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n4", "Number of obs", 100)))),
+                     column(9, textOutput("text4")))),
+          
+          tabPanel("Reproducible research",
+                   includeMarkdown("./content/05_Reproducible research.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n5", "Number of obs", 100)))),
+                     column(9, textOutput("text5")))),
+          
+          tabPanel("Statistical Inference",
+                   includeMarkdown("./content/06_Statistical Inference.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n6", "Number of obs", 100)))),
+                     column(9, textOutput("text6")))),
+          
+          tabPanel("Regression Models",
+                   includeMarkdown("./content/07_Regression Models.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n7", "Number of obs", 100)))),
+                     column(9, textOutput("text7")))),
+          
+          tabPanel("Practical Machine Learning",
+                   includeMarkdown("./content/08_Practical Machine Learning.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n8", "Number of obs", 100)))),
+                     column(9, textOutput("text8")))),
+          
+          tabPanel("Developing Data Products",
+                   includeMarkdown("./content/09_Developing Data Products.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n9", "Number of obs", 100)))),
+                     column(9, textOutput("text9")))),
+          
+          tabPanel("Data Science Capstone",
+                   includeMarkdown("./content/10_Data Science Capstone.md"),
+                   fluidRow(
+                     column(3,
+                            wellPanel(
+                              fluidRow(
+                                numericInput("n10", "Number of obs", 100)))),
+                     column(9, textOutput("text10"))))
+        )
+      )
     )
   )
 )
